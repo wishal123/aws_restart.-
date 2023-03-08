d26591c5ce7685ca4e6284c07a7563e4d951aeb1
wishal123 <127221801+wishal123@users.noreply.github.com>
2023-03-08 09:51:25 -0600

    Add files via upload

---
+++ aws.py
@@ -0,0 +1,8 @@
+Python 3.11.2 (v3.11.2:878ead1ac1, Feb  7 2023, 10:02:41) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello, World"'
+...       
+SyntaxError: incomplete input
+>>> print("Hello, World")
+...       
+Hello, World
